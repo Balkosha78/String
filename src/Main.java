9f09d3f96bdead9d42013d8eaf322be1a86aad04
Алексей <balko-aleksey@yandex.ru>
2024-09-19 21:24:17 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+}
+    public static void task1(){
+
+    }
+    public static void task2(){
+
+    }
+    public static void task3(){
+
+    }
+}
